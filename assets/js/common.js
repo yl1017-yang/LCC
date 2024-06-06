01135c5da56e4a702889ea13a174e81d9703bc9f
ylyu <yl.yu@pulmuone.com>
2024-06-06 10:51:27 +0900

    main

--- assets/js/common.js
+++ assets/js/common.js
@@ -82,45 +82,45 @@ function gnbMenu(depth1, depth2, depth3) {
     });
     
     // 모바일 - 1,2DEPTH 오픈
-    $moGnbLi.children('a').on('click', function(e) {
-    		// e.preventDefault();
+    // $moGnbLi.children('a').on('click', function(e) {
+    // 		// e.preventDefault();
         
-        var $depth = $(this).next('ul');
-        if($depth.is(':visible')){
-            $(this).removeClass('on');
-            $depth.slideUp(300);
-        } else {
-        		$moGnbLi.children('a').removeClass('on');
-            $(this).parent().siblings().find('ul').slideUp(300);
-            $(this).addClass('on');
-            $depth.slideDown(300);
-        }
-				$('html, body').animate({scrollTop:top_pos}); //강제닫힘현상 해결
-    });
+    //     var $depth = $(this).next('ul');
+    //     if($depth.is(':visible')){
+    //         $(this).removeClass('on');
+    //         $depth.slideUp(300);
+    //     } else {
+    //     		$moGnbLi.children('a').removeClass('on');
+    //         $(this).parent().siblings().find('ul').slideUp(300);
+    //         $(this).addClass('on');
+    //         $depth.slideDown(300);
+    //     }
+		// 		$('html, body').animate({scrollTop:top_pos}); //강제닫힘현상 해결
+    // });
     
-    $('.mo_gnb .depth1 > li').children('a').on('click', function() {
-        var $depth = $(this).next('ul');
-        if($depth.is(':visible')){
-        		$(this).removeClass('on');
-            $depth.slideUp(300);
-        } else {
-        		$('.mo_gnb .depth1 > li').children('a').removeClass('on');
-            $(this).parent().siblings().find('ul').slideUp(300);
-            $(this).addClass('on');
-            $depth.slideDown(300);
-        }
-				$('html, body').animate({scrollTop:top_pos}); //강제닫힘현상 해결
-    });
+    // $('.mo_gnb .depth1 > li').children('a').on('click', function() {
+    //     var $depth = $(this).next('ul');
+    //     if($depth.is(':visible')){
+    //     		$(this).removeClass('on');
+    //         $depth.slideUp(300);
+    //     } else {
+    //     		$('.mo_gnb .depth1 > li').children('a').removeClass('on');
+    //         $(this).parent().siblings().find('ul').slideUp(300);
+    //         $(this).addClass('on');
+    //         $depth.slideDown(300);
+    //     }
+		// 		$('html, body').animate({scrollTop:top_pos}); //강제닫힘현상 해결
+    // });
     
-    //모바일 - 페이지 인식
-		if ($moGnbLi.length > depth1-1) {
-        $moGnbLi.eq(depth1-1).find('> a').addClass('on');
-        $moGnbLi.eq(depth1-1).find('.depth1 > li').eq(depth2-1).find('> a').addClass('on');
-        $moGnbLi.eq(depth1-1).find('.depth1 > li').eq(depth2-1).find('.depth2 > li').eq(depth3-1).find('> a').addClass('on');
-		}
+    // //모바일 - 페이지 인식
+		// if ($moGnbLi.length > depth1-1) {
+    //     $moGnbLi.eq(depth1-1).find('> a').addClass('on');
+    //     $moGnbLi.eq(depth1-1).find('.depth1 > li').eq(depth2-1).find('> a').addClass('on');
+    //     $moGnbLi.eq(depth1-1).find('.depth1 > li').eq(depth2-1).find('.depth2 > li').eq(depth3-1).find('> a').addClass('on');
+		// }
     
-    //모바일 - 2depth 오픈
-    if ( depth1 >= 0 && depth2 >= 0 ) {
-        $moGnbLi.eq(depth1-1).addClass('on').children('.depth1').show().children('li').eq(depth2-1).addClass('on').children('.depth2').show();
-    }
+    // //모바일 - 2depth 오픈
+    // if ( depth1 >= 0 && depth2 >= 0 ) {
+    //     $moGnbLi.eq(depth1-1).addClass('on').children('.depth1').show().children('li').eq(depth2-1).addClass('on').children('.depth2').show();
+    // }
 }
